8868b8f47b602d5beafda032b09a2bff193596bc
ljleb <set>
2023-07-04 03:47:58 -0400

    backup

--- scripts/main.py
+++ scripts/main.py
@@ -34,26 +34,29 @@ class ActivationScript(scripts.Script):
                 save_activations = gr.Button(
                     value='Save activations',
                 )
-                reset_activations = gr.Button(
+                clear_activations = gr.Button(
                     value='Clear activations'
                 )
 
             save_activations.click(
                 fn=self.save_activations
             )
-            reset_activations.click(
-                fn=self.reset_activations
+            clear_activations.click(
+                fn=self.clear_activations
             )
 
         return [enabled, start_steps_ratio, end_steps_ratio]
 
     def save_activations(self):
+        print(fr'Saving activations to E:\sd\models\activation\unet.safetensors')
         safetensors.torch.save_file(global_state.activations, r'E:\sd\models\activation\unet.safetensors')
 
-    def reset_activations(self):
+    def clear_activations(self):
         global_state.activations.clear()
+        global_state.accumulation_steps.clear()
         with torch.no_grad():
             torch.cuda.empty_cache()
+        print(fr'Cleared activations')
 
     def process(self, p, enabled, start_steps_ratio, end_steps_ratio):
         global_state.enabled = enabled
@@ -64,7 +67,7 @@ class ActivationScript(scripts.Script):
         global_state.end_steps_ratio = end_steps_ratio
 
         for key, parameter in p.sd_model.named_parameters():
-            if 'model.diffusion_model' in key:
+            if is_backward_key(key):
                 if unwrap_key(key) not in global_state.activations:
                     global_state.activations[unwrap_key(key)] = torch.zeros_like(parameter.data, device=parameter.device)
 
@@ -73,12 +76,32 @@ class ActivationScript(scripts.Script):
 
         print('state dict len:', len(global_state.activations))
 
+    def process_batch(self, p, *args, **kwargs):
+        global_state.generation_total_steps = p.steps
+        global_state.generation_current_step = 0
+
     def postprocess(self, p, processed, *args):
         global_state.enabled = False
+        for key, parameter in p.sd_model.named_parameters():
+            if is_backward_key(key):
+                # TODO: restore old `requires_grad` and `retain_grad`
+                parameter.requires_grad = False
+                parameter.retain_grad = False
+
+        with torch.no_grad():
+            torch.cuda.empty_cache()
 
 
 def unwrap_key(key: str) -> str:
     return key.replace('.wrapped.', '.')
+
+
+def is_backward_key(key: str) -> bool:
+    return key.startswith(('model.diffusion_model', 'cond_stage_model')) and 'time_embed' not in key
+
+
+def is_root_key(key: str) -> bool:
+    return key in ('model.diffusion_model', 'cond_stage_model')
 
 
 @dataclasses.dataclass
@@ -93,34 +116,50 @@ class BackwardHook:
         self.no_grad = torch.set_grad_enabled(True)
         self.no_grad.__enter__()
 
-        model.zero_grad()
-
     def post_forward_callback(self, model, args, kwargs, output):
         if not global_state.enabled:
             return
 
-        if self.key == 'model.diffusion_model' and is_accumulation_step():
-            loss = output.sum()
+        if is_root_key(self.key) and is_enabled_accumulation_step():
+            loss = -output.mean()
             loss.backward()
 
             for key, parameter in shared.sd_model.named_parameters():
-                if not 'model.diffusion_model' in key:
+                if not is_backward_key(key) or not key.startswith(self.key):
                     continue
 
                 unwrapped_key = unwrap_key(key)
-                global_state.activations[unwrapped_key] = global_state.activations[unwrapped_key] * max(1, global_state.accumulation_count)
-                global_state.activations[unwrapped_key] += parameter.grad / shared.state.sampling_steps
-                global_state.activations[unwrapped_key] /= global_state.accumulation_count + 1
+                accumulation_step = global_state.accumulation_steps.get(unwrapped_key, 0)
+                global_state.accumulation_steps[unwrapped_key] = accumulation_step + 1
+                # dividing by `shared.state.sampling_steps` is wrong, needs to be the number of steps where `is_active_accumulation_step()` returns true
+                # global_state.activations[unwrapped_key] = global_state.activations[unwrapped_key] * max(1, accumulation_step)
+                # global_state.activations[unwrapped_key] += parameter.grad
+                # global_state.activations[unwrapped_key] /= accumulation_step + 1
+
+                # grad = torch.softmax(torch.flatten(parameter.grad), dim=0).reshape_as(parameter.grad)
+                # global_state.activations[unwrapped_key] **= accumulation_step / (accumulation_step + 1)
+                # global_state.activations[unwrapped_key] *= grad ** (1 / (accumulation_step + 1))
+                # if not torch.nonzero(global_state.activations[unwrapped_key]).any():
+                #     print('zero key', key)
+                # if torch.isnan(global_state.activations[unwrapped_key]).any():
+                #     print('nan key', key)
+
+                grad = parameter.grad
+                global_state.activations[unwrapped_key] **= accumulation_step / (accumulation_step + 1)
+                global_state.activations[unwrapped_key] *= grad ** (1 / (accumulation_step + 1))
+                if not torch.nonzero(global_state.activations[unwrapped_key]).any():
+                    print('zero key', key)
+                if torch.isnan(global_state.activations[unwrapped_key]).any():
+                    print('nan key', key)
 
             model.zero_grad()
-            global_state.accumulation_count += 1
 
         self.no_grad.__exit__(None, None, None)
         self.no_grad = None
 
 
-def is_accumulation_step():
-    current_steps_ratio = (shared.state.sampling_step + 1) / shared.state.sampling_steps
+def is_enabled_accumulation_step():
+    current_steps_ratio = global_state.generation_current_step / global_state.generation_total_steps
     return global_state.start_steps_ratio <= current_steps_ratio < global_state.end_steps_ratio
 
 
@@ -138,6 +177,13 @@ def on_model_loaded(sd_model):
 
 
 script_callbacks.on_model_loaded(on_model_loaded)
+
+
+def on_cfg_denoised(*args, **kwargs):
+    global_state.generation_current_step += 1
+
+
+script_callbacks.on_cfg_denoised(on_cfg_denoised)
 
 
 class TorchNoGradHijack:
